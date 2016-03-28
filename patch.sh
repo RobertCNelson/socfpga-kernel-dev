b12f86caeb741ca9071346eb334ac0b5035c2e6f
Robert Nelson <robertcnelson@gmail.com>
2016-03-28 12:10:40 -0500

    4.4.x: socfgpa git + rt
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ patch.sh
@@ -0,0 +1,207 @@
+#!/bin/sh -e
+#
+# Copyright (c) 2009-2016 Robert Nelson <robertcnelson@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Split out, so build_kernel.sh and build_deb.sh can share..
+
+. ${DIR}/version.sh
+if [ -f ${DIR}/system.sh ] ; then
+	. ${DIR}/system.sh
+fi
+
+#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
+unset git_opts
+git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
+if [ ! "x${git_no_edit}" = "x" ] ; then
+	git_opts="--no-edit"
+fi
+
+git="git am"
+git_patchset="https://github.com/altera-opensource/linux-socfpga/"
+#git_opts
+
+if [ "${RUN_BISECT}" ] ; then
+	git="git apply"
+fi
+
+echo "Starting patch.sh"
+
+git_add () {
+	git add .
+	git commit -a -m 'testing patchset'
+}
+
+start_cleanup () {
+	git="git am --whitespace=fix"
+}
+
+cleanup () {
+	if [ "${number}" ] ; then
+		git format-patch -${number} -o ${DIR}/patches/
+	fi
+	exit 2
+}
+
+cherrypick () {
+	if [ ! -d ../patches/${cherrypick_dir} ] ; then
+		mkdir -p ../patches/${cherrypick_dir}
+	fi
+	git format-patch -1 ${SHA} --start-number ${num} -o ../patches/${cherrypick_dir}
+	num=$(($num+1))
+}
+
+external_git () {
+	git_tag="socfpga-4.4"
+	echo "pulling: ${git_tag}"
+	git pull ${git_opts} ${git_patchset} ${git_tag}
+}
+
+aufs_fail () {
+	echo "aufs4 failed"
+	exit 2
+}
+
+aufs4 () {
+	echo "dir: aufs4"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-kbuild.patch
+		patch -p1 < aufs4-kbuild.patch || aufs_fail
+		rm -rf aufs4-kbuild.patch
+		git add .
+		git commit -a -m 'merge: aufs4-kbuild' -s
+
+		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-base.patch
+		patch -p1 < aufs4-base.patch || aufs_fail
+		rm -rf aufs4-base.patch
+		git add .
+		git commit -a -m 'merge: aufs4-base' -s
+
+		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-mmap.patch
+		patch -p1 < aufs4-mmap.patch || aufs_fail
+		rm -rf aufs4-mmap.patch
+		git add .
+		git commit -a -m 'merge: aufs4-mmap' -s
+
+		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-standalone.patch
+		patch -p1 < aufs4-standalone.patch || aufs_fail
+		rm -rf aufs4-standalone.patch
+		git add .
+		git commit -a -m 'merge: aufs4-standalone' -s
+
+		git format-patch -4 -o ../patches/aufs4/
+		exit 2
+	fi
+
+	${git} "${DIR}/patches/aufs4/0001-merge-aufs4-kbuild.patch"
+	${git} "${DIR}/patches/aufs4/0002-merge-aufs4-base.patch"
+	${git} "${DIR}/patches/aufs4/0003-merge-aufs4-mmap.patch"
+	${git} "${DIR}/patches/aufs4/0004-merge-aufs4-standalone.patch"
+
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		echo "dir: aufs4"
+
+		cd ../
+		if [ ! -f ./aufs4-standalone ] ; then
+			git clone https://github.com/sfjro/aufs4-standalone
+			cd ./aufs4-standalone
+			git checkout origin/aufs${KERNEL_REL} -b tmp
+			cd ../
+		fi
+		cd ./KERNEL/
+
+		cp -v ../aufs4-standalone/Documentation/ABI/testing/*aufs ./Documentation/ABI/testing/
+		mkdir -p ./Documentation/filesystems/aufs/
+		cp -rv ../aufs4-standalone/Documentation/filesystems/aufs/* ./Documentation/filesystems/aufs/
+		mkdir -p ./fs/aufs/
+		cp -v ../aufs4-standalone/fs/aufs/* ./fs/aufs/
+		cp -v ../aufs4-standalone/include/uapi/linux/aufs_type.h ./include/uapi/linux/
+
+		git add .
+		git commit -a -m 'merge: aufs4' -s
+		git format-patch -5 -o ../patches/aufs4/
+
+		exit 2
+	fi
+
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		start_cleanup
+	fi
+
+	${git} "${DIR}/patches/aufs4/0005-merge-aufs4.patch"
+
+	if [ "x${regenerate}" = "xenable" ] ; then
+		number=5
+		cleanup
+	fi
+}
+
+rt_cleanup () {
+	echo "rt: needs fixup"
+	exit 2
+}
+
+rt () {
+	echo "dir: rt"
+	rt_patch="${KERNEL_REL}${kernel_rt}"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		wget -c https://www.kernel.org/pub/linux/kernel/projects/rt/${KERNEL_REL}/patch-${rt_patch}.patch.xz
+		xzcat patch-${rt_patch}.patch.xz | patch -p1 || rt_cleanup
+		rm -f patch-${rt_patch}.patch.xz
+		rm -f localversion-rt
+		git add .
+		git commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -s
+		git format-patch -1 -o ../patches/rt/
+
+		exit 2
+	fi
+
+	${git} "${DIR}/patches/rt/0001-merge-CONFIG_PREEMPT_RT-Patch-Set.patch"
+}
+
+local_patch () {
+	echo "dir: dir"
+	${git} "${DIR}/patches/dir/0001-patch.patch"
+}
+
+external_git
+#aufs4
+rt
+#local_patch
+
+packaging () {
+	echo "dir: packaging"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		cp -v "${DIR}/3rdparty/packaging/builddeb" "${DIR}/KERNEL/scripts/package"
+		git commit -a -m 'packaging: sync builddeb changes' -s
+		git format-patch -1 -o "${DIR}/patches/packaging"
+		exit 2
+	else
+		${git} "${DIR}/patches/packaging/0001-packaging-sync-builddeb-changes.patch"
+	fi
+}
+
+packaging
+echo "patch.sh ran successfully"
---
+++ version.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+config="socfpga_defconfig"
+
+build_prefix="-socfpga-r"
+branch_prefix="v"
+branch_postfix=".x"
+
+#arm
+KERNEL_ARCH=arm
+#toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_gnueabi_4_6"
+#toolchain="gcc_linaro_gnueabihf_4_7"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+#toolchain="gcc_linaro_gnueabihf_4_9"
+toolchain="gcc_linaro_gnueabihf_5"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
+
+#Kernel/Build
+KERNEL_REL=4.4
+KERNEL_TAG=${KERNEL_REL}.4
+BUILD=${build_prefix}0
+kernel_rt=".4-rt11"
+
+#v4.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
+#KERNEL_SHA=""
+
+#git branch
+BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
+
+DISTRO=cross
+DEBARCH=armhf
+#
