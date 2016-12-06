363599217f262e7b408cd78a7f07926a9a67c27e
Robert Nelson <robertcnelson@gmail.com>
2016-12-06 11:40:35 -0600

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 #
 # Copyright (c) 2009-2016 Robert Nelson <robertcnelson@gmail.com>
 #
@@ -22,54 +22,78 @@
 
 # Split out, so build_kernel.sh and build_deb.sh can share..
 
+shopt -s nullglob
+
 . ${DIR}/version.sh
 if [ -f ${DIR}/system.sh ] ; then
 	. ${DIR}/system.sh
 fi
+git_bin=$(which git)
+#git hard requirements:
+#git: --no-edit
 
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
-
-git="git am"
+git="${git_bin} am"
+#git_patchset=""
 git_patchset="https://github.com/altera-opensource/linux-socfpga/"
 #git_opts
 
 if [ "${RUN_BISECT}" ] ; then
-	git="git apply"
+	git="${git_bin} apply"
 fi
 
 echo "Starting patch.sh"
 
 git_add () {
-	git add .
-	git commit -a -m 'testing patchset'
+	${git_bin} add .
+	${git_bin} commit -a -m 'testing patchset'
 }
 
 start_cleanup () {
-	git="git am --whitespace=fix"
+	git="${git_bin} am --whitespace=fix"
 }
 
 cleanup () {
 	if [ "${number}" ] ; then
 		if [ "x${wdir}" = "x" ] ; then
-			git format-patch -${number} -o ${DIR}/patches/
+			${git_bin} format-patch -${number} -o ${DIR}/patches/
 		else
-			git format-patch -${number} -o ${DIR}/patches/${wdir}/
+			if [ ! -d ${DIR}/patches/${wdir}/ ] ; then
+				mkdir -p ${DIR}/patches/${wdir}/
+			fi
+			${git_bin} format-patch -${number} -o ${DIR}/patches/${wdir}/
 			unset wdir
 		fi
 	fi
 	exit 2
 }
 
+dir () {
+	wdir="$1"
+	if [ -d "${DIR}/patches/$wdir" ]; then
+		echo "dir: $wdir"
+
+		if [ "x${regenerate}" = "xenable" ] ; then
+			start_cleanup
+		fi
+
+		number=
+		for p in "${DIR}/patches/$wdir/"*.patch; do
+			${git} "$p"
+			number=$(( $number + 1 ))
+		done
+
+		if [ "x${regenerate}" = "xenable" ] ; then
+			cleanup
+		fi
+	fi
+	unset wdir
+}
+
 cherrypick () {
 	if [ ! -d ../patches/${cherrypick_dir} ] ; then
 		mkdir -p ../patches/${cherrypick_dir}
 	fi
-	git format-patch -1 ${SHA} --start-number ${num} -o ../patches/${cherrypick_dir}
+	${git_bin} format-patch -1 ${SHA} --start-number ${num} -o ../patches/${cherrypick_dir}
 	num=$(($num+1))
 }
 
@@ -80,7 +104,8 @@ external_git () {
 
 	git_tag="socfpga-4.5"
 	echo "pulling: ${git_tag}"
-	git pull ${git_opts} ${git_patchset} ${git_tag}
+	${git_bin} pull --no-edit ${git_patchset} ${git_tag}
+	${git_bin} describe
 }
 
 aufs_fail () {
@@ -95,45 +120,40 @@ aufs4 () {
 		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-kbuild.patch
 		patch -p1 < aufs4-kbuild.patch || aufs_fail
 		rm -rf aufs4-kbuild.patch
-		git add .
-		git commit -a -m 'merge: aufs4-kbuild' -s
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs4-kbuild' -s
 
 		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-base.patch
 		patch -p1 < aufs4-base.patch || aufs_fail
 		rm -rf aufs4-base.patch
-		git add .
-		git commit -a -m 'merge: aufs4-base' -s
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs4-base' -s
 
 		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-mmap.patch
 		patch -p1 < aufs4-mmap.patch || aufs_fail
 		rm -rf aufs4-mmap.patch
-		git add .
-		git commit -a -m 'merge: aufs4-mmap' -s
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs4-mmap' -s
 
 		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-standalone.patch
 		patch -p1 < aufs4-standalone.patch || aufs_fail
 		rm -rf aufs4-standalone.patch
-		git add .
-		git commit -a -m 'merge: aufs4-standalone' -s
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs4-standalone' -s
 
-		git format-patch -4 -o ../patches/aufs4/
-		exit 2
-	fi
-
-	${git} "${DIR}/patches/aufs4/0001-merge-aufs4-kbuild.patch"
-	${git} "${DIR}/patches/aufs4/0002-merge-aufs4-base.patch"
-	${git} "${DIR}/patches/aufs4/0003-merge-aufs4-mmap.patch"
-	${git} "${DIR}/patches/aufs4/0004-merge-aufs4-standalone.patch"
-
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		echo "dir: aufs4"
+		${git_bin} format-patch -4 -o ../patches/aufs4/
 
 		cd ../
-		if [ ! -f ./aufs4-standalone ] ; then
-			git clone https://github.com/sfjro/aufs4-standalone
+		if [ ! -d ./aufs4-standalone ] ; then
+			${git_bin} clone https://github.com/sfjro/aufs4-standalone
 			cd ./aufs4-standalone
-			git checkout origin/aufs${KERNEL_REL} -b tmp
+			${git_bin} checkout origin/aufs${KERNEL_REL} -b tmp
+			cd ../
+		else
+			rm -rf ./aufs4-standalone || true
+			${git_bin} clone https://github.com/sfjro/aufs4-standalone
+			cd ./aufs4-standalone
+			${git_bin} checkout origin/aufs${KERNEL_REL} -b tmp
 			cd ../
 		fi
 		cd ./KERNEL/
@@ -145,9 +165,9 @@ aufs4 () {
 		cp -v ../aufs4-standalone/fs/aufs/* ./fs/aufs/
 		cp -v ../aufs4-standalone/include/uapi/linux/aufs_type.h ./include/uapi/linux/
 
-		git add .
-		git commit -a -m 'merge: aufs4' -s
-		git format-patch -5 -o ../patches/aufs4/
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs4' -s
+		${git_bin} format-patch -5 -o ../patches/aufs4/
 
 		exit 2
 	fi
@@ -157,12 +177,17 @@ aufs4 () {
 		start_cleanup
 	fi
 
+	${git} "${DIR}/patches/aufs4/0001-merge-aufs4-kbuild.patch"
+	${git} "${DIR}/patches/aufs4/0002-merge-aufs4-base.patch"
+	${git} "${DIR}/patches/aufs4/0003-merge-aufs4-mmap.patch"
+	${git} "${DIR}/patches/aufs4/0004-merge-aufs4-standalone.patch"
 	${git} "${DIR}/patches/aufs4/0005-merge-aufs4.patch"
 	${git} "${DIR}/patches/aufs4/0006-aufs-call-mutex.owner-only-when-DEBUG_MUTEXES-or-MUT.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		git format-patch -6 -o ../patches/aufs4/
-		exit 2
+		wdir="aufs4"
+		number=6
+		cleanup
 	fi
 }
 
@@ -180,9 +205,9 @@ rt () {
 		xzcat patch-${rt_patch}.patch.xz | patch -p1 || rt_cleanup
 		rm -f patch-${rt_patch}.patch.xz
 		rm -f localversion-rt
-		git add .
-		git commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -s
-		git format-patch -1 -o ../patches/rt/
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -s
+		${git_bin} format-patch -1 -o ../patches/rt/
 
 		exit 2
 	fi
@@ -200,13 +225,66 @@ aufs4
 #rt
 #local_patch
 
+pre_backports () {
+	echo "dir: backports/${subsystem}"
+
+	cd ~/linux-src/
+	${git_bin} pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master
+	${git_bin} pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master --tags
+	${git_bin} pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master --tags
+	if [ ! "x${backport_tag}" = "x" ] ; then
+		${git_bin} checkout ${backport_tag} -b tmp
+	fi
+	cd -
+}
+
+post_backports () {
+	if [ ! "x${backport_tag}" = "x" ] ; then
+		cd ~/linux-src/
+		${git_bin} checkout master -f ; ${git_bin} branch -D tmp
+		cd -
+	fi
+
+	${git_bin} add .
+	${git_bin} commit -a -m "backports: ${subsystem}: from: linux.git" -s
+	if [ ! -d ../patches/backports/${subsystem}/ ] ; then
+		mkdir -p ../patches/backports/${subsystem}/
+	fi
+	${git_bin} format-patch -1 -o ../patches/backports/${subsystem}/
+
+	exit 2
+}
+
+patch_backports (){
+	echo "dir: backports/${subsystem}"
+	${git} "${DIR}/patches/backports/${subsystem}/0001-backports-${subsystem}-from-linux.git.patch"
+}
+
+backports () {
+	backport_tag="v4.x-y"
+
+	subsystem="xyz"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/x/ ./x/
+
+		post_backports
+	fi
+	patch_backports
+}
+
+###
+#backports
+
 packaging () {
 	echo "dir: packaging"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 		cp -v "${DIR}/3rdparty/packaging/builddeb" "${DIR}/KERNEL/scripts/package"
-		git commit -a -m 'packaging: sync builddeb changes' -s
-		git format-patch -1 -o "${DIR}/patches/packaging"
+		${git_bin} commit -a -m 'packaging: sync builddeb changes' -s
+		${git_bin} format-patch -1 -o "${DIR}/patches/packaging"
 		exit 2
 	else
 		${git} "${DIR}/patches/packaging/0001-packaging-sync-builddeb-changes.patch"
--- tools/host_det.sh
+++ tools/host_det.sh
@@ -3,6 +3,8 @@
 #opensuse support added by: Antonio Cavallo
 #https://launchpad.net/~a.cavallo
 
+git_bin=$(which git)
+
 warning () { echo "! $@" >&2; }
 error () { echo "* $@" >&2; exit 1; }
 info () { echo "+ $@" >&2; }
@@ -71,7 +73,7 @@ redhat_reqs () {
 		echo "RPM distro version: [${rpm_distro}]"
 
 		case "${rpm_distro}" in
-		22|23|24)
+		22|23|24|25)
 			pkgtool="dnf"
 			;;
 		esac
@@ -124,12 +126,16 @@ check_dpkg () {
 
 debian_regs () {
 	unset deb_pkgs
+	pkg="bash"
+	check_dpkg
 	pkg="bc"
 	check_dpkg
 	pkg="build-essential"
 	check_dpkg
-	pkg="device-tree-compiler"
-	check_dpkg
+	if ! type dtc >/dev/null; then
+		pkg="device-tree-compiler"
+		check_dpkg
+	fi
 	pkg="fakeroot"
 	check_dpkg
 	pkg="lsb-release"
@@ -139,6 +145,9 @@ debian_regs () {
 	pkg="lzop"
 	check_dpkg
 	pkg="man-db"
+	check_dpkg
+	#git
+	pkg="gettext"
 	check_dpkg
 
 	unset warn_dpkg_ia32
@@ -331,6 +340,11 @@ debian_regs () {
 			#http://blog.linuxmint.com/?p=2975
 			deb_distro="xenial"
 			;;
+		serena)
+			#18.1
+			#http://packages.linuxmint.com/index.php
+			deb_distro="xenial"
+			;;
 		esac
 
 		#Future Debian Code names:
@@ -414,13 +428,31 @@ debian_regs () {
 		wheezy|precise)
 			pkg="libncurses5-dev"
 			check_dpkg
+			if [ "x${build_git}" = "xtrue" ] ; then
+				#git
+				pkg="libcurl4-gnutls-dev"
+				check_dpkg
+				pkg="libexpat1-dev"
+				check_dpkg
+				pkg="libssl-dev"
+				check_dpkg
+			fi
 			;;
 		*)
 			pkg="libncurses5-dev:${deb_arch}"
 			check_dpkg
+			if [ "x${build_git}" = "xtrue" ] ; then
+				#git
+				pkg="libcurl4-gnutls-dev:${deb_arch}"
+				check_dpkg
+				pkg="libexpat1-dev:${deb_arch}"
+				check_dpkg
+				pkg="libssl-dev:${deb_arch}"
+				check_dpkg
+			fi
 			;;
 		esac
-		
+
 		#pkg: ia32-libs
 		if [ "x${deb_arch}" = "xamd64" ] ; then
 			unset dpkg_multiarch
@@ -472,7 +504,7 @@ debian_regs () {
 		echo "-----------------------------"
 		echo "Please cut, paste and email to: bugs@rcn-ee.com"
 		echo "-----------------------------"
-		echo "git: [$(git rev-parse HEAD)]"
+		echo "git: [$(${git_bin} rev-parse HEAD)]"
 		echo "git: [$(cat .git/config | grep url | sed 's/\t//g' | sed 's/ //g')]"
 		echo "uname -m: [$(uname -m)]"
 		echo "lsb_release -a:"
@@ -498,11 +530,11 @@ BUILD_HOST=${BUILD_HOST:="$( detect_host )"}
 if [ "$(which lsb_release)" ] ; then
 	info "Detected build host [$(lsb_release -sd)]"
 	info "host: [$(uname -m)]"
-	info "git HEAD commit: [$(git rev-parse HEAD)]"
+	info "git HEAD commit: [$(${git_bin} rev-parse HEAD)]"
 else
 	info "Detected build host [$BUILD_HOST]"
 	info "host: [$(uname -m)]"
-	info "git HEAD commit: [$(git rev-parse HEAD)]"
+	info "git HEAD commit: [$(${git_bin} rev-parse HEAD)]"
 fi
 
 DIR=$PWD
@@ -520,10 +552,40 @@ if [ "x${ARCH}" = "xx86_64" ] ; then
 	gcc_linaro_eabi_5|gcc_linaro_gnueabihf_5|gcc_linaro_aarch64_gnu_5)
 		ignore_32bit="true"
 		;;
+	gcc_linaro_eabi_6|gcc_linaro_gnueabihf_6|gcc_linaro_aarch64_gnu_6)
+		ignore_32bit="true"
+		;;
 	*)
 		ignore_32bit="false"
 		;;
 	esac
+fi
+
+git_bin=$(which git)
+
+git_major=$(LC_ALL=C ${git_bin} --version | awk '{print $3}' | cut -d. -f1)
+git_minor=$(LC_ALL=C ${git_bin} --version | awk '{print $3}' | cut -d. -f2)
+git_sub=$(LC_ALL=C ${git_bin} --version | awk '{print $3}' | cut -d. -f3)
+
+#debian Stable:
+#https://packages.debian.org/stable/git -> 2.1.4
+
+compare_major="2"
+compare_minor="1"
+compare_sub="4"
+
+unset build_git
+
+if [ "${git_major}" -lt "${compare_major}" ] ; then
+	build_git="true"
+elif [ "${git_major}" -eq "${compare_major}" ] ; then
+	if [ "${git_minor}" -lt "${compare_minor}" ] ; then
+		build_git="true"
+	elif [ "${git_minor}" -eq "${compare_minor}" ] ; then
+		if [ "${git_sub}" -lt "${compare_sub}" ] ; then
+			build_git="true"
+		fi
+	fi
 fi
 
 case "$BUILD_HOST" in
